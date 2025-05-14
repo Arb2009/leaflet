ce709a5e349e03349c6059d2a3fd40428a8c29e5
Arb2009 <arbsmajlaj10@gmail.com>
2025-05-14 16:53:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
-// var map = L.map('map').setView([42.660607117451065, 21.16330624770136], 13);
+var Crust  = L.marker([42.65244897157923, 21.17176212807544]).bindPopup('Ketu eshte Pizza Crust.'),
+    Napoli = L.marker([42.653479732786735, 21.16771501628135]).bindPopup('Ketu eshte Napoli Pizza.'),
+    Yana   = L.marker([42.66308122788367, 21.16153853530947]).bindPopup('Ketu eshte Yana Napolitana.'),
+    Ruds   = L.marker([42.66050732833732, 21.161825886478464]).bindPopup('Ketu eshte Ruds Pizza.');
+    Sos    = L.marker([42.651026823391724, 21.168499103734533]).bindPopup('Ketu eshte Sos Pizza.');
 
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 19,
-//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-// }).addTo(map);
+    var cities = L.layerGroup([Crust, Napoli, Yana, Ruds, Sos]);
 
-// L.marker([42.541630, 20.286049]).addTo(map);
+    var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap'
+    });
+    
+    var osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'});
+    
+        var map = L.map('map', {
+            center: [42.667332, 21.164184],
+            zoom: 10,
+            layers: [osm, cities]
+        });
 
+        var baseMaps = {
+            "OpenStreetMap": osm,
+            "OpenStreetMap.HOT": osmHOT
+        };
+        
+        var overlayMaps = {
+            "Pizza": cities
+        };
 
+        var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
 
-var littleton = L.marker([42.6330125010515, 21.15192155374466]).bindPopup('This is Albi Mall.'),
-    denver    = L.marker([42.56468276478752, 21.133789251128654]).bindPopup('This is Prishtina Mall.'),
+        var baseMaps = {
+            "OpenStreetMap": osm,
+            "<span style='color: red'>OpenStreetMap.HOT</span>": osmHOT
+        };
+
+        var crownHill = L.marker([42.662201628299336, 21.168809471278458]).bindPopup('This is Parku i Qytetit.'),
+    rubyHill = L.marker([42.67543245693771, 21.211067145369682]).bindPopup('This is Parku i Germise.');
+    
+var parks = L.layerGroup([crownHill, rubyHill]);
+var openTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: 'Map data: © OpenStreetMap contributors, SRTM | Map style: © OpenTopoMap (CC-BY-SA)'
+});
+
+layerControl.addBaseLayer(openTopoMap, "OpenTopoMap");
+layerControl.addOverlay(parks, "Hamburger");var littleton = L.marker([42.65243020394924, 21.171770102331546]).bindPopup('This is Albi Mall.'),
+    denver    = L.marker([42.653479732786735, 21.16771501628135]).bindPopup('This is Prishtina Mall.'),
     aurora    = L.marker([42.65662848419542, 21.160342895463245]).bindPopup('This is Katedralja Nene Tereza.'),
     golden    = L.marker([42.659574, 21.160663]).bindPopup('This is Sheshi Zahir Pajaziti.');
 
@@ -47,14 +85,4 @@ var littleton = L.marker([42.6330125010515, 21.15192155374466]).bindPopup('This 
             "<span style='color: red'>OpenStreetMap.HOT</span>": osmHOT
         };
 
-        var crownHill = L.marker([42.662201628299336, 21.168809471278458]).bindPopup('This is Parku i Qytetit.'),
-    rubyHill = L.marker([42.67543245693771, 21.211067145369682]).bindPopup('This is Parku i Germise.');
-    
-var parks = L.layerGroup([crownHill, rubyHill]);
-var openTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: 'Map data: © OpenStreetMap contributors, SRTM | Map style: © OpenTopoMap (CC-BY-SA)'
-});
-
-layerControl.addBaseLayer(openTopoMap, "OpenTopoMap");
-layerControl.addOverlay(parks, "Parks");
+      
