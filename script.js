494881cac64ee6936c9dae670c712351b9233c61
Arb2009 <arbsmajlaj10@gmail.com>
2025-05-21 16:55:22 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var Crust  = L.marker([42.65244897157923, 21.17176212807544]).bindPopup('Ketu eshte Pizza Crust.'),
-    Napoli = L.marker([42.653479732786735, 21.16771501628135]).bindPopup('Ketu eshte Napoli Pizza.'),
-    Yana   = L.marker([42.66308122788367, 21.16153853530947]).bindPopup('Ketu eshte Yana Napolitana.'),
-    Ruds   = L.marker([42.66050732833732, 21.161825886478464]).bindPopup('Ketu eshte Ruds Pizza.'),
+var Crust  = L.marker([42.65244897157923, 21.17176212807544]).bindPopup('Ketu eshte Pizza Crust <br> Vleresimet 4.5 - 5 yje <br> Ale S thot Restoranti i bukur ka shumë mundësi për pica, sanduiç, makarona dhe shumë ushqime të këndshme dhe të shijshme.'),
+    Napoli = L.marker([42.653479732786735, 21.16771501628135]).bindPopup('Ketu eshte Napoli Pizza <br> Vleresimet 4.5 - 5 yje <br>Anyle Zogu thot Një nga picabërësit më të mirë të Prishtinës. Kopshti i tyre është gjithashtu i bukur sidomos gjatë orës së artë.'),
+    Jana   = L.marker([42.66308122788367, 21.16153853530947]).bindPopup('Ketu eshte Jana Napolitana <br> Vleresimet 4.8 - 5 yje <br>Diella Sejfijaj thot Ky lokal ofron një ambient të dekoruar me shumë shije dhe kujdes, duke krijuar një atmosferë të ngrohtë dhe familjare. Stafi është jashtëzakonisht mikpritës, duke e bërë eksperiencën akoma më të këndshme .'),
+    Ruds   = L.marker([42.66050732833732, 21.161825886478464]).bindPopup('Ketu eshte Ruds Pizza <br> Vleresimet 4.6 - 5 yje <br> Kastriot Mucolli thot Pica e tyre është tepër e shijshme, me një kore krokante por të butë, salcë të pasur domatesh dhe sasinë e duhur të djathit..'),
     Sos    = L.marker([42.651026823391724, 21.168499103734533]).bindPopup('Ketu eshte Sos Pizza.');
 
-var pizza = L.layerGroup([Crust, Napoli, Yana, Ruds, Sos]);
+var pizza = L.layerGroup([Crust, Napoli, Jana, Ruds, Sos]);
 
 // hamburger
 var Aba = L.marker([42.6495931071088, 21.157716800469704]).bindPopup('Ketu eshte Hamburger Aba.'),
@@ -23,14 +23,17 @@ var MammaMia = L.marker([42.65333057210587, 21.16757118455793]).bindPopup('Ketu 
 sette = L.marker([42.6580731391406, 21.166656956444875]).bindPopup('Ketu eshte SETTE-Ristorante Italiano.');
 ponte = L.marker([42.66434685415317, 21.15938891553721]).bindPopup('Ketu eshte Ponte Vecchio.');
 la = L.marker([42.656035778513825, 21.175143053395796]).bindPopup('Ketu eshte La Napoletana.');
+trattoria = L.marker([42.64850207720944, 21.171007998102567]).bindPopup('Ketu eshte Restaurant Trattoria.');
+vapiano = L.marker([42.658653731872185, 21.15500649763883]).bindPopup('Ketu eshte Vapiano.');
 
-var italian = L.layerGroup([MammaMia, BellaItalia, sette, ponte, la]);
+var italian = L.layerGroup([MammaMia, BellaItalia, sette, ponte, la, trattoria, vapiano]);
 
 // Greek restaurants
-var AthensGrill = L.marker([42.654000, 21.166000]).bindPopup('Ketu eshte Athens Grill.'),
-    SantoriniTaverna = L.marker([42.659000, 21.162000]).bindPopup('Ketu eshte Santorini Taverna.');
+var trock = L.marker([42.66184666155505, 21.154231161087417]).bindPopup('Ketu eshte Trock Taverna.'),
+    baba = L.marker([42.65936927110693, 21.159702867145086]).bindPopup('Ketu eshte Baba Ganoush Meze.');
+gagi = L.marker([42.6635813816616, 21.162249957004892]).bindPopup('Ketu eshte Gagi Restaurant <br> Rating.');
 
-var greek = L.layerGroup([AthensGrill, SantoriniTaverna]);
+var greek = L.layerGroup([trock, baba, gagi]);
 
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
